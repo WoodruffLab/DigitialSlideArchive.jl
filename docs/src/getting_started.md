1efdb96d8a03828f860cfe8cb310a470560f5e95
Kevin Bonham <kevin@bonham.ch>
2024-09-11 12:03:28 -0400

    working on more of the girder API, fix python import

--- docs/src/getting_started.md
+++ docs/src/getting_started.md
@@ -12,9 +12,52 @@ In order to interact with your DSA server, you will need
 1. An API url. Typically, this is the url of your server followed by `/api/v1`,
    eg. `http://multiplex.pathology.emory.edu:8080/api/v1`
 2. An API key. This is what allows you to authenticate with the server.
-   See the next section if you need an API key.
+   See the [the authentication section](#getting-an-api-key) if you need an API key.
 
-### Getting an API key
+## Set-up
+
+Once you have your URL and [api key](#getting-an-api-key),
+set the environment variables `DSA_API_URL` and `DSA_API_KEY`.
+
+The way to do this depends on your shell.
+If you are a mac user, your default shell is probably `zsh`,
+and you can set environment variables using `export`, eg
+
+```zsh
+export DSA_API_URL="http://multiplex.pathology.emory.edu:8080/api/v1"
+export DSA_API_KEY="your-key-here"
+```
+
+This can be done in your shell before launching julia,
+or these lines can be added to your `~/.zshrc` file.
+Alternatively (and this is recommended),
+use something like [`direnv`](https://direnv.net)
+to set these variables when you are running code from within a particular directory.
+
+!!! note
+    You can instead pass the URL and key strings
+    to the [`DSAClient()`](@ref) functions
+    directly, but this is not recommended,
+    since if this code gets shared, it could reveal your API key
+    to other users, giving them full access to your server and data.
+
+### Connecting to the DSA girder client
+
+Don't worry if you don't know what `girder` is -
+it's just the underlying code that makes it easy for the server
+(where the DSA is running) and the client (your computer) to communicate.
+
+To enable this communication, use the [`DSAClient()`](@ref) function.
+If you have the environmental variables set,
+you can simply call the function without arguments -
+but be sure to assign it to a variable
+```julia
+import DigitalSlideArchive as DSA
+
+client = DSAClient()
+```
+
+## Getting an API key
 
 With an account on a DSA server,
 click on your user name in the upper right hand corner,
@@ -32,10 +75,9 @@ and copy the alphanumeric text.
 
 ![Get API key](assets/api_key2.avif)
 
-!> [!WARNING]
-> This Key enables anyone to access the server and perform all actions that you can.
-> DO NOT share this key or add it to any publicly accessible code.
-
+!!! warning
+    This Key enables anyone to access the server and perform all actions that you can.
+    DO NOT share this key or add it to any publicly accessible code.
 
 ```@index
 ```
--- src/DigitalSlideArchive.jl
+++ src/DigitalSlideArchive.jl
@@ -1,39 +1,25 @@
 module DigitalSlideArchive
 
-export DSAClient
+export DSAClient,
+       DSAResponse,
+       get_item,
+       get_files
 
 using PythonCall
-girder_client = pyimport("girder_client")
+
+const girder_client = Ref{Py}()
+
+function __init__()
+girder_client[] = pyimport("girder_client")
+end
+
 
 """
-    DSAClient
+    DSAClient(; url, key)
 
 This is a thin wrapper around the `girder_client.GirderClient`
-type provided by the `girder_client` python package.
-
-The best way to create this is to use [the `DSA.client` function](@ref DigitalSlideArchive.client)
-"""
-struct DSAClient
-    api_url
-    client
-end
-
-function Base.getproperty(dsa_client::DSAClient, field::Symbol)
-    if field == :api_url
-        getfield(dsa_client, :field)
-    else
-        getproperty(dsa_client.client, field)
-    end
-end
-
-Base.propertynames(dsa_client::DSAClient) = vcat([:api_url, :client], propertynames(dsa_client.client))
-
-
-"""
-    client(; url, key)
-
-Create a girder client object that can be used
-for interactiving with a DSA server.
+type provided by the `girder_client` python package
+that can be used for interactiving with a DSA server.
 
 A url for the API is required, but can be set
 with the `DSA_API_URL` environmental variable.
@@ -42,59 +28,105 @@ this can be added using the `ENV` dictionary.
 Eg.
 
 ```julia
-using DigitalSlideArchive as DSA
-gc = DSA.client(; url = "http://multiplex.pathology.emory.edu:8080/api/v1")
+using DigitalSlideArchive
+client = DSAClient(; url = "http://multiplex.pathology.emory.edu:8080/api/v1")
 
 # OR:
 
 ENV["DSA_API_URL"] = "http://multiplex.pathology.emory.edu:8080/api/v1"
-gc = DSA.client()
+client = DSAClient()
 ```
 
 If the `key` argument is provided (not recommended, see [`authenticate!`](@ref)),
 or the `DSA_API_KEY` variable is set,
 the client will automatically be authenticated.
 """
-function client(; url = ENV["DSA_API_URL"], key=get(ENV, "DSA_API_KEY", nothing))
-    gc = girder_client.GirderClient(; url)
-    dsa_client = DSAClient(url, gc)
-    !isnothing(key) && authenticate!(dsa_client; key)
-    return dsa_client
+struct DSAClient
+    api_url
+    client
+
+    function DSAClient(; url=ENV["DSA_API_URL"], key=get(ENV, "DSA_API_KEY", nothing))
+        gc = girder_client[].GirderClient(; apiUrl = url)
+        dsa_client = new(url, gc)
+        !isnothing(key) && authenticate!(dsa_client; key)
+        return dsa_client
+    end
 end
 
-# TODO: use Preferences.jl and/or set up user-input using TerminalMenus
+
+function Base.getproperty(dsa_client::DSAClient, field::Symbol)
+    if field ∈ (:api_url, :client)
+        getfield(dsa_client, field)
+    else
+        getproperty(dsa_client.client, field)
+    end
+end
+
+Base.propertynames(dsa_client::DSAClient) = vcat([:api_url, :client], propertynames(dsa_client.client))
+
+struct DSAResponse
+    resp::Dict
+end
+
+function DSAResponse(resp::PythonCall.Core.Py)
+    DSAResponse(pyconvert(Dict, resp))
+end
+
+Base.getindex(resp::DSAResponse, key) = resp.resp[key]
+Base.get(resp::DSAResponse, key, default=nothing) = get(resp.resp, key, default)
+function Base.show(io::IO, ::MIME"text/plain", resp::DSAResponse)
+    println(io, "DSAResponse containing:")
+    show(io, MIME"text/plain"(), resp.resp)
+end
 
 """
-    authenticate!(client; key)
+    authenticate!(client::DSAClient; key)
 
 Authenticate the girder client using the provided API `key`.
 By default, this will look for the enviroment variable `DSA_API_KEY`.
 In principle, you can pass this in your code or add it to `ENV`
-as described in [`DSA.client`](@ref DigitalSlideArchive.client), but this is not recommended,
+as described in [`DSAClient`](@ref), but this is not recommended,
 since it's quite easy to accidentally commit that code.
 
-!!! warn
+!!! warning
     Anyone that has your API key may have full access to all
     data on your DSA server, including the ability to modify or delete it!
     Make sure to keep this plain-text key secure.
 
 
 ```julia
-using DigitalSlideArchive as DSA
-gc = DSA.client(; url = "http://multiplex.pathology.emory.edu:8080/api/v1")
-DSA.authenticate!(gc)
+client = DSAClient(; url = "http://multiplex.pathology.emory.edu:8080/api/v1")
+DigitalSlideArchive.authenticate!(gc)
 ```
+
+!!! note
+    When creating a [`DSAClient`](@ref), if an API key is provided
+    (either in the constructor or using the environmental variable)
+    the authentication will happen automatically.
 """
 function authenticate!(dsa_client::DSAClient; key = ENV["DSA_API_KEY"])
-    dsa_client.auth!(; apiKey=key )
+    dsa_client.authenticate(; apiKey=key )
     return dsa_client
 end
 
 # TODO: use Preferences.jl and/or set up user-input using TerminalMenus
 
 """
-    get_folders(client)
+    get_item(client, item)
+
+Get item with specified `item` "_id".
+"""
+get_item(client::DSAClient, item) = DSAResponse(client.get("item/$item"))
+
 
 """
+    get_files(client, item)
 
+Get files stored in `item` "_id".
+"""
+function get_files(client::DSAClient, item)
+    resp = client.get("item/$item/files")
+    [DSAResponse(v) for v in pyconvert(Vector, resp)]
 end
+
+end # module DigitalSlideArchive
